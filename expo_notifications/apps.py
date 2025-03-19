a030d0b78fabd4a7a8ee26654ba44868145d1b19
Jonathan Ehwald <github@ehwald.info>
2025-03-20 00:59:55 +0100

    Set verbose app name

--- expo_notifications/apps.py
+++ expo_notifications/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ExpoNotificationsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "expo_notifications"
+    verbose_name = "Expo Notifications"

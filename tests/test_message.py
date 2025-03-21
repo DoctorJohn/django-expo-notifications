ac49ff7fa161ebc218be03d07b62dedb60cb877a
Jonathan Ehwald <github@ehwald.info>
2025-03-21 15:09:01 +0100

    Test passing through of fields during conversion

--- tests/test_message.py
+++ tests/test_message.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 import pytest
 
+from expo_notifications.models import Message
 from tests.factories import MessageFactory
 
 
@@ -26,6 +27,13 @@ def test_to_push_message_converts_blank_data_to_none():
 
 
 @pytest.mark.django_db
+def test_to_push_message_passes_data_through():
+    message = MessageFactory(data={"key": "value"})
+    push_message = message.to_push_message()
+    assert push_message.data == {"key": "value"}
+
+
+@pytest.mark.django_db
 def test_to_push_message_converts_blank_title_to_none():
     message = MessageFactory(title="")
     push_message = message.to_push_message()
@@ -33,10 +41,24 @@ def test_to_push_message_converts_blank_title_to_none():
 
 
 @pytest.mark.django_db
+def test_to_push_message_passes_title_through():
+    message = MessageFactory(title="Test Title")
+    push_message = message.to_push_message()
+    assert push_message.title == "Test Title"
+
+
+@pytest.mark.django_db
 def test_to_push_message_converts_blank_body_to_none():
     message = MessageFactory(body="")
     push_message = message.to_push_message()
     assert push_message.body is None
+
+
+@pytest.mark.django_db
+def test_to_push_message_passes_body_through():
+    message = MessageFactory(body="Test Body")
+    push_message = message.to_push_message()
+    assert push_message.body == "Test Body"
 
 
 @pytest.mark.django_db
@@ -82,11 +104,26 @@ def test_to_push_message_converts_blank_priority_to_none():
     assert push_message.priority is None
 
 
+@pytest.mark.parametrize("priority", [p for p, _ in Message.PRIORITY_CHOICES])
+@pytest.mark.django_db
+def test_to_push_message_passes_priority_through(priority):
+    message = MessageFactory(priority=priority)
+    push_message = message.to_push_message()
+    assert push_message.priority == priority
+
+
 @pytest.mark.django_db
 def test_to_push_message_converts_blank_subtitle_to_none():
     message = MessageFactory(subtitle="")
     push_message = message.to_push_message()
     assert push_message.subtitle is None
+
+
+@pytest.mark.django_db
+def test_to_push_message_passes_subtitle_through():
+    message = MessageFactory(subtitle="Test Subtitle")
+    push_message = message.to_push_message()
+    assert push_message.subtitle == "Test Subtitle"
 
 
 @pytest.mark.django_db
@@ -97,10 +134,25 @@ def test_to_push_message_converts_blank_sound_to_none():
 
 
 @pytest.mark.django_db
+def test_to_push_message_passes_sound_through():
+    message = MessageFactory(sound="default")
+    push_message = message.to_push_message()
+    assert push_message.sound == "default"
+
+
+@pytest.mark.django_db
 def test_to_push_message_converts_blank_badge_to_none():
     message = MessageFactory(badge=None)
     push_message = message.to_push_message()
     assert push_message.badge is None
+
+
+@pytest.mark.parametrize("badge", [0, 1, 2, 10])
+@pytest.mark.django_db
+def test_to_push_message_passes_badge_through(badge):
+    message = MessageFactory(badge=badge)
+    push_message = message.to_push_message()
+    assert push_message.badge == badge
 
 
 @pytest.mark.django_db
@@ -111,7 +163,29 @@ def test_to_push_message_converts_blank_channel_id_to_none():
 
 
 @pytest.mark.django_db
+def test_to_push_message_passes_channel_id_through():
+    message = MessageFactory(channel_id="Default")
+    push_message = message.to_push_message()
+    assert push_message.channel_id == "Default"
+
+
+@pytest.mark.django_db
 def test_to_push_message_converts_blank_category_id_to_none():
     message = MessageFactory(category_id="")
     push_message = message.to_push_message()
     assert push_message.category is None
+
+
+@pytest.mark.django_db
+def test_to_push_message_passes_category_id_through():
+    message = MessageFactory(category_id="Test Category")
+    push_message = message.to_push_message()
+    assert push_message.category == "Test Category"
+
+
+@pytest.mark.parametrize("mutable_content", [True, False])
+@pytest.mark.django_db
+def test_to_push_message_passes_mutable_content_through(mutable_content):
+    message = MessageFactory(mutable_content=mutable_content)
+    push_message = message.to_push_message()
+    assert push_message.mutable_content == mutable_content
